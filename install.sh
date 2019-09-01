45116a98a589c9022ea91b77fd676381b7a72100
and-gorbik <and-gorbik@mail.ru>
2019-09-01 15:14:48 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 echo "> network setup"
+read
 cat interfaces > /etc/network/interfaces
 service networking restart
-
 echo "ping to the host"
 ping -c 3 192.168.56.1
-read
 
 echo "> packages upgrading"
-apt-get update && apt-get upgrade -y
 read
+apt-get update >> /var/log/update_script.log
+apt-get upgrade -y >> /var/log/update_script.log
 
 echo "> installation of all necessary packages"
+read
 while read PKG
     do apt-get install -y $PKG
 done < requirements
-read
 
 echo "> adding a new sudo user"
+read
 echo "sudoer name:"
 read NAME
 echo "password:"
@@ -26,35 +27,31 @@ read PASSWD
 . sudoer.sh
 sudoer $NAME $PASSWD
 tail -n 1 /etc/passwd
-read
 
 echo "> ssh setup for user $NAME"
+read
 cat sshd_config >> /etc/ssh/sshd_config
 service sshd restart
-read
 
 echo "> services disabling"
+read
 while read SERVICE
     do systemctl disable $SERVICE
 done < disabled_services
-read
 
 echo "> crontab setup"
+read
 cp update.sh /root/update.sh
 chmod +x /root/update.sh
 echo "0 4 */7 * * /root/update.sh" >> /etc/crontab
 echo "@reboot /root/update.sh" >> /etc/crontab
-read
 
 cp notifier.sh /root/notifier.sh
 chmod +x /root/notifier.sh
 echo "@daily /root/notifier.sh" >> /etc/crontab
-read
+
 
 # echo "root: $NAME"  >> /etc/aliases
 # newaliases
 
-    
-
-
-
+echo "Done!"
