a92e171d2b0afddb3ea2978f6ea01ca17208d791
and-gorbik <and-gorbik@mail.ru>
2019-10-05 14:44:07 +0300

    update iptables

--- iptables.sh
+++ iptables.sh
@@ -12,12 +12,6 @@ iptables -X
 iptables -t nat -X
 iptables -t mangle -X
 
-# deny invalid packages
-
-# deny bad tcp packages
-
-# deny spoofing
-
 # deny portscan
 iptables -A INPUT -m recent --rcheck --seconds 120 --name FUCKOFF -j DROP
 iptables -A INPUT -p tcp -m multiport ! --dports $SSH_PORT,80,443 -m recent --set --name FUCKOFF -j DROP
@@ -33,6 +27,16 @@ iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT ACCEPT
 
+# deny invalid packages
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP  
+
+# deny null packages
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+# deny bad tcp packages
+iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+iptables -A OUTPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+
 # allow lo
 iptables -A INPUT -i lo -j ACCEPT
 
@@ -42,6 +46,9 @@ iptables -A INPUT -p udp -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 # allow ssh
 iptables -A INPUT -p tcp -i $DEV -s $HOST_IP --dport $SSH_PORT -j ACCEPT
+
+# allow safe icmp messages
+# iptables -A INPUT -p icmp -icmp-type 3,8,12 -j ACCEPT
 
 # allow http, https for web-server
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
